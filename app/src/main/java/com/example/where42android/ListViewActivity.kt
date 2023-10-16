fee8ac428573b03cdf1aaf9563490eb80bc72678
JaeYoonJung08 <78781625+JaeYoonJung08@users.noreply.github.com>
2023-10-16 18:03:40 +0900

    screen transition page

---
+++ app/src/main/java/com/example/where42android/ListViewActivity.kt
@@ -0,0 +1,14 @@
+package com.example.where42android
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class ListViewActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+
+        }
+}
