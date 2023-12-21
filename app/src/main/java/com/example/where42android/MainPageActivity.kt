e2b69ed462ded7b5e6b6383979df62504d156ee2
JaeYoonJung08 <78781625+JaeYoonJung08@users.noreply.github.com>
2023-12-21 16:04:51 +0900

    feat : 그룹 리스트 RecyclerView로 변경

--- app/src/main/java/com/example/where42android/MainPageActivity.kt
+++ app/src/main/java/com/example/where42android/MainPageActivity.kt
@@ -19,181 +19,250 @@ import android.widget.ArrayAdapter
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.where42android.ApiObject.getMemberService
+import com.example.where42android.ApiObject.service
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Callback
-
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class MainPageActivity : AppCompatActivity() {
-//
-//    var groupProfileList = arrayListOf<profile_list>(
-//        profile_list("Jaeyojun","handsome", "퇴근", "profile_photo_example",),
-//        profile_list("Jooypark","beautiful", "퇴근", "profile_photo_example" ),
-//        profile_list("jaju","graphics master", "개포 c2r5s6", "profile_photo_example"),
-//    )
-//
-//    var friendProfileList = arrayListOf<profile_list>(
-//        profile_list("Jaeyojun","handsome", "개포 c2r5s6", "profile_photo_example",),
-//        profile_list("Jaeyojun","handsome", "퇴근", "profile_photo_example",),
-//        profile_list("Jaeyojun","handsome", "개포 c2r5s6", "profile_photo_example",),
-//        profile_list("Jooypark","beautiful", "개포 c2r5s6", "profile_photo_example" ),
-//        profile_list("jaju","graphics master", "퇴근", "profile_photo_example"),
-//    )
-//
-//    var isFilterChecked = false
-//
-//    var isGroupListVisible = false
-//    var isFriendListVisible = true
+
+    var groupProfileList = arrayListOf<profile_list>(
+        profile_list("Jaeyojun","handsome", "퇴근", "profile_photo_example",),
+        profile_list("Jooypark","beautiful", "퇴근", "profile_photo_example" ),
+        profile_list("jaju","graphics master", "개포 c2r5s6", "profile_photo_example"),
+    )
+
+
+    var friendProfileList = arrayListOf<profile_list>(
+        profile_list("Jaeyojun","handsome", "개포 c2r5s6", "profile_photo_example",),
+        profile_list("Jaeyojun","handsome", "퇴근", "profile_photo_example",),
+        profile_list("Jaeyojun","handsome", "개포 c2r5s6", "profile_photo_example",),
+        profile_list("Jooypark","beautiful", "개포 c2r5s6", "profile_photo_example" ),
+        profile_list("jaju","graphics master", "퇴근", "profile_photo_example"),
+    )
+
+    var isFilterChecked = false
+    var isGroupListVisible = false
+    var isFriendListVisible = true
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main_page)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+        //12_18 사용자 프로필 가져오기
+        val intraId = 6 // Replace with the actual intraId
 
-    //api 예제 모든 데이터르 들고와보자
-     val listView: ListView = findViewById(R.id.listView) // 리스트뷰의 ID를 적절하게 변경해야 합니다.
-            val adapter = ArrayAdapter<Member>(this, android.R.layout.simple_list_item_1)
-            listView.adapter = adapter
-
-        getMemberService.getMembers().enqueue(object : Callback<List<Member>> {
-            override fun onResponse(call: Call<List<Member>>, response: Response<List<Member>>) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = service.getMember(intraId)
+            withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
-                    val members = response.body() ?: return
-                    adapter.clear()
-                    adapter.addAll(members)
+                    val member = response.body()
+                    // TODO: Handle the member data
+                    if (member != null) {
+                        val intraIdTextView =
+                            findViewById<TextView>(R.id.intra_id) // XML에서 TextView 식별
+                        intraIdTextView.text =
+                            member.intraName // member에서 Intra ID를 가져와 TextView에 설정
+                        val comment = findViewById<TextView>(R.id.Comment)
+                        comment.text = member.comment
+                        val location = findViewById<TextView>(R.id.location_info)
+                        location.text = member.location
+                        val main_image = findViewById<CircleImageView>(R.id.profile_photo)
+                        val imageUrl = member.image
+                        Glide.with(this@MainPageActivity)
+                            .load(imageUrl)
+                            .apply(RequestOptions().circleCrop()) // CircleImageView를 사용하므로 원형으로 자르기 위해 circleCrop을 적용합니다.
+                            .into(main_image)
+                    }
                 } else {
-                    Log.e("API Error", "Failed to get members: ${response.errorBody()}")
-                    // 서버에서 에러 응답을 받았을 때의 처리를 작성합니다.
+                    Log.e("API Error", "Failed to get member: ${response.errorBody()}")
                 }
             }
+        }
 
-            override fun onFailure(call: Call<List<Member>>, t: Throwable) {
-                Log.e("API Error", "Failed to get members", t)
-                // 네트워크 요청이 실패했을 때의 처리를 작성합니다.
+
+        fun getDeviceScreenSize(context: Context): Pair<Int, Int> {
+            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            val display = windowManager.defaultDisplay
+
+            val metrics = DisplayMetrics()
+            display.getMetrics(metrics)
+
+            val widthPixels = metrics.widthPixels
+            val heightPixels = metrics.heightPixels
+
+            return Pair(widthPixels, heightPixels)
+        }
+
+        val screenSize = getDeviceScreenSize(this)
+        val screenWidth = screenSize.first
+        val screenHeight = screenSize.second
+
+// 너비와 높이 출력
+        Log.d("ScreenSize", "Width: $screenWidth pixels, Height: $screenHeight pixels")
+
+
+        /* header의 환경 설정 버튼을 눌렀을 때 */
+        val settingButton: ImageButton = this.findViewById(R.id.setting_button)
+        settingButton.setOnClickListener {
+            try {
+                val intent = Intent(this, SettingPage::class.java)
+                startActivity(intent)
+                finish()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "환경 세팅 작업을 수행하는 동안 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
             }
-        })
-    }
-}
+
+        }
+
+        /*새 그룹 기능 구현*/
+        val newGroupButton: Button = this.findViewById(R.id.newGroupButton)
+
+        newGroupButton.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.new_group_popup)
+
+            dialog.setCanceledOnTouchOutside(true)
+            dialog.setCancelable(true)
+            dialog.window?.setGravity(Gravity.CENTER)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            val editText = dialog.findViewById<EditText>(R.id.input)
+            val btnCancel = dialog.findViewById<Button>(R.id.cancel)
+            val btnSubmit = dialog.findViewById<Button>(R.id.submit)
+
+            btnCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            btnSubmit.setOnClickListener {
+                val userInput = editText.text.toString()
+                val intent = Intent(this, CreateGroupActivity::class.java)
+                intent.putExtra("userInputKey", userInput)
+                startActivity(intent)
+            }
+            dialog.show()
+        }
+
+        /* footer의 홈버튼과 검색 버튼 기능 구현 */
+        val searchButton: ImageButton = this.findViewById(R.id.search_button)
+
+        searchButton.setOnClickListener {
+            try {
+                //Toast.makeText(this, "버튼을 클릭했습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, SearchPage::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "작업을 수행하는 동안 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val homeButton: ImageButton = this.findViewById(R.id.home_button)
+
+        homeButton.setOnClickListener {
+            try {
+                if (this::class.java != MainPageActivity::class.java) {
+                    val intent = Intent(this, MainPageActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else
+                    Toast.makeText(this, "이미 로그인 페이지에 있습니다!.", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "작업을 수행하는 동안 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
+        /* rcyclerView 기능 구현*/
+        //12_18 리사이클 뷰 넣어주기
+        //api 예제 모든 데이터르 들고와보자
+//        val listView: ListView = findViewById(R.id.listView) // 리스트뷰의 ID를 적절하게 변경해야 합니다.
+//        val adapter = ArrayAdapter<Member>(this, android.R.layout.simple_list_item_1)
+//        listView.adapter = adapter
 //
-//        fun getDeviceScreenSize(context: Context): Pair<Int, Int> {
-//            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-//            val display = windowManager.defaultDisplay
-//
-//            val metrics = DisplayMetrics()
-//            display.getMetrics(metrics)
-//
-//            val widthPixels = metrics.widthPixels
-//            val heightPixels = metrics.heightPixels
-//
-//            return Pair(widthPixels, heightPixels)
-//        }
-//
-//        val screenSize = getDeviceScreenSize(this)
-//        val screenWidth = screenSize.first
-//        val screenHeight = screenSize.second
-//
-//// 너비와 높이 출력
-//        Log.d("ScreenSize", "Width: $screenWidth pixels, Height: $screenHeight pixels")
-//
-//
-//        /* header의 환경 설정 버튼을 눌렀을 때 */
-//        val settingButton : ImageButton = this.findViewById(R.id.setting_button)
-//        settingButton.setOnClickListener{
-//            try {
-//                val intent = Intent(this, SettingPage::class.java)
-//                startActivity(intent)
-//                finish()
-//            }
-//            catch (e: Exception){
-//                e.printStackTrace()
-//                Toast.makeText(this, "환경 세팅 작업을 수행하는 동안 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
-//            }
-//
-//        }
-//
-//        /*새 그룹 기능 구현*/
-//        val newGroupButton: Button = this.findViewById(R.id.newGroupButton)
-//
-//        newGroupButton.setOnClickListener {
-//            val dialog = Dialog(this)
-//            dialog.setContentView(R.layout.new_group_popup)
-//
-//            dialog.setCanceledOnTouchOutside(true)
-//            dialog.setCancelable(true)
-//            dialog.window?.setGravity(Gravity.CENTER)
-//            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//
-//            val editText = dialog.findViewById<EditText>(R.id.input)
-//            val btnCancel = dialog.findViewById<Button>(R.id.cancel)
-//            val btnSubmit = dialog.findViewById<Button>(R.id.submit)
-//
-//            btnCancel.setOnClickListener {
-//                dialog.dismiss()
-//            }
-//            btnSubmit.setOnClickListener {
-//                val userInput = editText.text.toString()
-//                val intent = Intent(this, CreateGroupActivity::class.java)
-//                intent.putExtra("userInputKey", userInput)
-//                startActivity(intent)
-//            }
-//            dialog.show()
-//        }
-//
-//        /* footer의 홈버튼과 검색 버튼 기능 구현 */
-//        val searchButton: ImageButton = this.findViewById(R.id.search_button)
-//
-//        searchButton.setOnClickListener {
-//            try {
-//                //Toast.makeText(this, "버튼을 클릭했습니다.", Toast.LENGTH_SHORT).show()
-//                val intent = Intent(this, SearchPage::class.java)
-//                startActivity(intent)
-//            }catch (e: Exception) {
-//                e.printStackTrace()
-//                Toast.makeText(this, "작업을 수행하는 동안 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//
-//        val homeButton: ImageButton = this.findViewById(R.id.home_button)
-//
-//        homeButton.setOnClickListener {
-//            try {
-//                if (this::class.java != MainPageActivity::class.java) {
-//                    val intent = Intent(this, MainPageActivity::class.java)
-//                    startActivity(intent)
-//                    finish()
+//        getMemberService.getMembers().enqueue(object : Callback<List<Member>> {
+//            override fun onResponse(call: Call<List<Member>>, response: Response<List<Member>>) {
+//                if (response.isSuccessful) {
+//                    val members = response.body() ?: return
+//                    adapter.clear()
+//                    adapter.addAll(members)
+//                } else {
+//                    Log.e("API Error", "Failed to get members: ${response.errorBody()}")
+//                    // 서버에서 에러 응답을 받았을 때의 처리를 작성합니다.
 //                }
-//                else
-//                    Toast.makeText(this, "이미 로그인 페이지에 있습니다!.", Toast.LENGTH_SHORT).show()
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//                Toast.makeText(this, "작업을 수행하는 동안 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
 //            }
-//        }
+//            override fun onFailure(call: Call<List<Member>>, t: Throwable) {
+//                Log.e("API Error", "Failed to get members", t)
+//                // 네트워크 요청이 실패했을 때의 처리를 작성합니다.
+//            }})
+
+//-------------------------------------
+//        여기가 그룹임
+//        val recyclerView: RecyclerView = findViewById(R.id.all_group)
+//        val adapter = RecyclerViewAdapterAll(this)
+//        recyclerView.adapter = adapter
+
+//        val recyclerView: RecyclerView = findViewById(R.id.all_group)
+//        val adapter = RecyclerViewAdapterAll(this)
+//        recyclerView.adapter = adapter
+
+        val recyclerView: RecyclerView = findViewById(R.id.all_group)
+        val adapter = RecyclerViewAdapterAll(this)
+
+        // LinearLayoutManager를 설정해야 합니다.
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        recyclerView.adapter = adapter
+
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+//        val recyclerView: RecyclerView = findViewById(R.id.all_group)
+//        val layoutManager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = layoutManager
 //
+//// Adapter 초기화 및 RecyclerView에 연결
+//        val groupProfileList = arrayListOf<profile_list>(
+//            profile_list("Jaeyojun","handsome", "퇴근", "profile_photo_example"),
+//            profile_list("Jooypark","beautiful", "퇴근", "profile_photo_example"),
+//            // ... (원하는 데이터 계속 추가)
+//        )
 //
-//        /* rcyclerView 기능 구현*/
+//        val adapter = RecyclerViewAdapterAll(this, groupProfileList, isFilterChecked)
+//        recyclerView.adapter = adapter
+
+        //----------------------------------------
 //        val groupRecyclerView: RecyclerView = findViewById<RecyclerView?>(R.id.group_list)
 //        val groupRecyclerViewAdapter = RecyclerViewAdapter(this, groupProfileList, isFilterChecked)
 //        groupRecyclerView.layoutManager = LinearLayoutManager(this)
 //        groupRecyclerView.adapter = groupRecyclerViewAdapter
-//
-//
+
+
 //        val friendRecyclerView: RecyclerView = findViewById<RecyclerView?>(R.id.friend_list)
 //        val friendRecyclerViewAdapter = RecyclerViewAdapter(this, friendProfileList, isFilterChecked)
 //        friendRecyclerView.layoutManager = LinearLayoutManager(this)
 //        friendRecyclerView.adapter = friendRecyclerViewAdapter
-//
+
 //        val checkBox: CheckBox = findViewById(R.id.checkBox)
 //        checkBox.setOnCheckedChangeListener { _, isChecked ->
 //            isFilterChecked = isChecked
 //            groupRecyclerView.adapter = RecyclerViewAdapter(this, groupProfileList, isFilterChecked)
-//            friendRecyclerView.adapter = RecyclerViewAdapter(this, friendProfileList, isFilterChecked)
+////            friendRecyclerView.adapter = RecyclerViewAdapter(this, friendProfileList, isFilterChecked)
 //        }
 //
 //        val groupEditButton: ImageButton = findViewById(R.id.group_edit)
@@ -211,7 +280,7 @@ class MainPageActivity : AppCompatActivity() {
 //            }
 //            isGroupListVisible = !isGroupListVisible
 //        }
-//
+
 //        val friendToggleButton: ImageButton = findViewById(R.id.friend_toggle)
 //        friendToggleButton.setOnClickListener {
 //            if (isFriendListVisible) {
@@ -223,5 +292,5 @@ class MainPageActivity : AppCompatActivity() {
 //        }
 
         /* 그룹 설정 기능 */
-//    }
-//}
+    }
+}
---
+++ app/src/main/java/com/example/where42android/RecyclerViewAdapterAll.kt
@@ -0,0 +1,166 @@
+package com.example.where42android
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class RecyclerViewAdapterAll(private val context: Context) :
+    RecyclerView.Adapter<RecyclerViewAdapterAll.ViewHolder>() {
+
+    private val dummyDataList = mutableListOf<String>()
+
+    init {
+        for (i in 1..10) {
+            dummyDataList.add("Group $i")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recycler_group_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val groupName = dummyDataList[position]
+        holder.groupName.text = groupName
+        // 데이터를 ViewHolder의 View에 바인딩하는 로직이 필요합니다.
+        // 예: holder.groupName.text = groupName
+    }
+
+    override fun getItemCount(): Int {
+        return dummyDataList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val groupName: TextView = itemView.findViewById(R.id.group_name_text_view)
+    }
+}
+
+
+//class RecyclerViewAdapterAll(private val context: Context) :
+//    RecyclerView.Adapter<RecyclerViewAdapterAll.ViewHolder>() {
+//
+//    private val dummyDataList = mutableListOf<String>()
+//
+//    init {
+//        // 10개의 더미 데이터 추가
+//        for (i in 1..10) {
+//            dummyDataList.add("Group $i")
+//        }
+//    }
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val view = LayoutInflater.from(context).inflate(R.layout.recycler_group_list, parent, false)
+//        return ViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        val groupName = dummyDataList[position]
+//        holder.groupName.text = groupName
+//        // 편집 버튼 클릭 리스너 설정
+////        holder.editButton.setOnClickListener {
+////            // 편집 버튼 클릭 이벤트 처리
+////            // 원하는 동작 수행
+////        }
+////
+////        // 토글 버튼 클릭 리스너 설정
+////        holder.toggleButton.setOnClickListener {
+////            // 토글 버튼 클릭 이벤트 처리
+////            // 원하는 동작 수행
+////        }
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return dummyDataList.size
+//    }
+//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val groupName: TextView = itemView.findViewById(R.id.group_name_text_view)
+////        val editButton: ImageButton = itemView.findViewById(R.id.group_edit)
+////        val toggleButton: ImageButton = itemView.findViewById(R.id.group_toggle)
+//    }
+//}
+
+
+
+//
+//import android.content.Context
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.ImageButton
+//import android.widget.ImageView
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class RecyclerViewAdapterAll (val context: Context, private var profileList: ArrayList<profile_list>, val filterChecked: Boolean) :
+//    RecyclerView.Adapter<RecyclerViewAdapterAll.ProfileViewHolder>() {
+//
+//
+//    fun updateList(newList: ArrayList<profile_list>) {
+//        profileList = newList
+//        notifyDataSetChanged()
+//    }
+//    inner class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+//        val profilePhoto: ImageView = itemView.findViewById(R.id.profile_photo)
+//        val intraId: TextView = itemView.findViewById(R.id.intra_id)
+//        val comment: TextView = itemView.findViewById(R.id.Comment)
+//        val locationInfo: TextView = itemView.findViewById(R.id.location_info)
+//        val seeMoreButton: ImageButton = itemView.findViewById(R.id.see_more)
+//
+//        init {
+//            seeMoreButton.setOnClickListener {
+//                val position = adapterPosition
+//                if (position != RecyclerView.NO_POSITION) {
+//                    val profile: profile_list
+//                    if (filterChecked) {
+//                        val filteredList = profileList.filter { it.location != "퇴근" }
+//                        profile = filteredList[position]
+//                    } else {
+//                        profile = profileList[position]
+//                    }
+//                    val profileDialog = ProfileDialog(context)
+//                    profileDialog.showProfileDialog(profile)
+//                }
+//            }
+//        }
+//    }
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+//        val view = LayoutInflater.from(context).inflate(R.layout.list_view_detail, parent, false)
+//        return ProfileViewHolder(view)
+//    }
+//    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+//        if (filterChecked) {
+//            // filterChecked가 true일 때만 필터링을 적용합니다.
+//            // '퇴근'인 요소를 건너뜁니다.
+//            val filteredList = profileList.filter { it.location != "퇴근" }
+//            val profile = filteredList[position]
+//            val resourceId = context.resources.getIdentifier(profile.photo, "drawable", context.packageName)
+//            holder.profilePhoto.setImageResource(resourceId)
+//            holder.intraId.text = profile.intraId
+//            holder.comment.text = profile.comment
+//            holder.locationInfo.text = profile.location
+//            // 여기서는 필터링된 목록을 사용합니다.
+//        } else {
+//            // filterChecked가 false이면 필터링하지 않고 원래 목록을 사용합니다.
+//            val profile = profileList[position]
+//            val resourceId = context.resources.getIdentifier(profile.photo, "drawable", context.packageName)
+//            holder.profilePhoto.setImageResource(resourceId)
+//            holder.intraId.text = profile.intraId
+//            holder.comment.text = profile.comment
+//            holder.locationInfo.text = profile.location
+//        }
+//    }
+//    override fun getItemCount(): Int {
+//        return if (filterChecked) {
+//            profileList.filter { it.location != "퇴근" }.size
+//        } else {
+//            profileList.size
+//        }
+//    }
+//
+//}
