07ac2bbe379eb1e9f459f9f26737e135af4275c1
JaeYoonJung08 <78781625+JaeYoonJung08@users.noreply.github.com>
2024-03-16 16:54:04 +0900

    feat : 버전 코드, 버전 네임 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.seoul.where42android"
         minSdk = 26 //31
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
