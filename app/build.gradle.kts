be8f1b8d8ef84124f5751768f34ba28e7dd40f21
JaeYoonJung08 <78781625+JaeYoonJung08@users.noreply.github.com>
2024-03-30 18:53:02 +0900

    feat : versionCode 9로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.seoul.where42android"
         minSdk = 26 //31
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
