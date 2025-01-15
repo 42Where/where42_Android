407c8a413999a7f39288bd0f24988a9b44f9b91a
JaeYoonJung08 <whitecy01@naver.com>
2025-01-15 14:07:02 +0900

    feat: SearchPage검색 속도 증가, TokenManager 에러 수정

--- app/src/main/java/com/seoul/where42android/main/TokenManager.kt
+++ app/src/main/java/com/seoul/where42android/main/TokenManager.kt
@@ -1,6 +1,7 @@
 package com.seoul.where42android.utils
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
@@ -86,8 +87,15 @@ object TokenManager {
      * DataStore에서 IntraId 가져오기
      */
     suspend fun getIntraId(): Int? {
-        val preferences = appContext.dataStoreInstance.data.firstOrNull()
-        return preferences?.get(INTRA_ID)
+//        val preferences = appContext.dataStoreInstance.data.firstOrNull()
+//        return preferences?.get(INTRA_ID)
+        if (!::appContext.isInitialized) {
+            Log.e("TokenManager", "TokenManager is not initialized")
+            return null
+        }
+        return runBlocking {
+            appContext.dataStoreInstance.data.firstOrNull()?.get(INTRA_ID)
+        }
     }
 
     /**
---
+++ app/src/main/java/com/seoul/where42android/main/v3/MainCompass.kt
@@ -0,0 +1,16 @@
+package com.seoul.where42android.main.v3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.seoul.where42android.R
+
+class MainCompass : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compass)
+
+
+
+    }
+}
